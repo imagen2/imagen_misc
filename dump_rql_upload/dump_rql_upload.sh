515ed057a30da2911d0c174971b435b19bfc48a0
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2019-05-14 10:36:16 +0200

    rlq → rql

--- dump_rql_upload/dump_rql_upload.sh
+++ dump_rql_upload/dump_rql_upload.sh
@@ -3,4 +3,4 @@
 DIRNAME=`dirname $0`
 
 . ~/virtualenvs/imagen_upload/bin/activate
-cubicweb-ctl shell imagen_upload "$DIRNAME/dump_rql_upload.py" > /chroot/data/tmp/cati/dump_rlq_upload.json
+cubicweb-ctl shell imagen_upload "$DIRNAME/dump_rql_upload.py" > /chroot/data/tmp/cati/dump_rql_upload.json
