1614cabce5c4127a16beaccf3cee58e1c41c448e
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2019-10-10 20:18:36 +0200

    No need to concatenate LimeSurvey files any more

--- crontab/imagen_psytools.sh
+++ crontab/imagen_psytools.sh
@@ -13,6 +13,6 @@ mkdir -p /tmp/imagen/BL/processed/psytools \
          /tmp/imagen/FU3/processed/psytools \
          /tmp/imagen/STRATIFY/processed/psytools
 /volatile/src/imagen_r/R/imagen_psytools_derive.R
-/volatile/src/imagen_databank/psytools/imagen_psytools_derive.py
+#/volatile/src/imagen_databank/psytools/imagen_psytools_derive.py
 /volatile/src/imagen_databank/psytools/imagen_psytools_deidentify.py
 rm -rf /tmp/imagen
