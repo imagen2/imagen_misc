81edd4c1418f93252f7fb53dae56c4ebe31caa30
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2020-06-22 13:45:01 +0200

    COVID-19 questionnaires
    Both for Imagen and Stratify cohorts.

--- crontab/imagen_psytools.sh
+++ crontab/imagen_psytools.sh
@@ -11,7 +11,11 @@ mkdir -p /tmp/imagen/BL/processed/psytools \
          /tmp/imagen/FU1/processed/psytools \
          /tmp/imagen/FU2/processed/psytools \
          /tmp/imagen/FU3/processed/psytools \
-         /tmp/imagen/STRATIFY/processed/psytools
+         /tmp/imagen/STRATIFY/processed/psytools \
+         /tmp/imagen/IMACOV19_BL/processed/psytools \
+         /tmp/imagen/IMACOV19_FU/processed/psytools \
+         /tmp/imagen/STRATICO19_BL/processed/psytools \
+         /tmp/imagen/STRATICO19_FU/processed/psytools
 /volatile/src/imagen_r/R/imagen_psytools_derive.R
 /volatile/src/imagen_databank/psytools/imagen_psytools_deidentify.py
 rm -rf /tmp/imagen
