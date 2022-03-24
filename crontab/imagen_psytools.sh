af30fea7ee3cc0ec794f51d35f728cca118b04c9
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2022-03-24 09:45:58 +0100

    COVID-19 follow-up 4 (2022) surveys
    Both for Imagen and Stratify/ESTRA cohorts.

--- crontab/imagen_psytools.sh
+++ crontab/imagen_psytools.sh
@@ -16,10 +16,12 @@ mkdir -p /tmp/imagen/BL/processed/psytools \
          /tmp/imagen/IMACOV19_FU/processed/psytools \
          /tmp/imagen/IMACOV19_FU2/processed/psytools \
          /tmp/imagen/IMACOV19_FU3/processed/psytools \
+         /tmp/imagen/IMACOV19_FU4/processed/psytools \
          /tmp/imagen/STRATICO19_BL/processed/psytools \
          /tmp/imagen/STRATICO19_FU/processed/psytools \
          /tmp/imagen/STRATICO19_FU2/processed/psytools \
-         /tmp/imagen/STRATICO19_FU3/processed/psytools
+         /tmp/imagen/STRATICO19_FU3/processed/psytools \
+         /tmp/imagen/STRATICO19_FU4/processed/psytools
 /volatile/src/imagen_r/R/imagen_psytools_derive.R
 /volatile/src/imagen_databank/psytools/imagen_psytools_deidentify.py
 rm -rf /tmp/imagen
